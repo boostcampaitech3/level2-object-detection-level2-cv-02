615fd1bdd68223e58bd9766f67f9cef06722bce8
Juyoung Kim <ingressor531@gmail.com>
2022-03-29 15:49:40 +0900

    Prediction with low threshold 전용 코드 업로드

---
+++ configs/thr_down/modified_swin_base_thr_down.py
@@ -0,0 +1,153 @@
+# Modified from
+# https://github.com/SwinTransformer/Swin-Transformer-Object-Detection/blob/master/configs/swin/cascade_mask_rcnn_swin_base_patch4_window7_mstrain_480-800_giou_4conv1f_adamw_3x_coco.py
+
+_base_ = [
+    '/opt/ml/detection/swin/configs/thr_down/cascade_mask_rcnn_swin_fpn_thr_down.py', # Modified
+    '/opt/ml/detection/swin/configs/coco_simillar_dataset.py', # Modified
+    '/opt/ml/detection/swin/configs/schedule_1x.py', # Modified
+    '/opt/ml/detection/swin/configs/default_runtime.py' # Modified
+]
+
+work_dir = '/opt/ml/detection/swin'
+pretrained = '/opt/ml/detection/swin/configs/cascade_mask_rcnn_swin_base_patch4_window7.pth'
+
+model = dict(
+    init_cfg=dict(type='Pretrained', checkpoint=pretrained), # Modified
+    backbone=dict(
+        embed_dim=128,
+        depths=[2, 2, 18, 2],
+        num_heads=[4, 8, 16, 32],
+        window_size=7,
+        ape=False,
+        drop_path_rate=0.3,
+        patch_norm=True,
+        use_checkpoint=False
+    ),
+    neck=dict(in_channels=[128, 256, 512, 1024]),
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='ConvFCBBoxHead',
+                num_shared_convs=4,
+                num_shared_fcs=1,
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10, # Modified
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.1, 0.1, 0.2, 0.2]),
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                norm_cfg=dict(type='BN', requires_grad=True), # Modified
+                loss_cls=dict(
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type='GIoULoss', loss_weight=10.0)),
+            dict(
+                type='ConvFCBBoxHead',
+                num_shared_convs=4,
+                num_shared_fcs=1,
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10, # Modified
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.05, 0.05, 0.1, 0.1]),
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                norm_cfg=dict(type='BN', requires_grad=True), # Modified
+                loss_cls=dict(
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type='GIoULoss', loss_weight=10.0)),
+            dict(
+                type='ConvFCBBoxHead',
+                num_shared_convs=4,
+                num_shared_fcs=1,
+                in_channels=256,
+                conv_out_channels=256,
+                fc_out_channels=1024,
+                roi_feat_size=7,
+                num_classes=10, # Modified
+                bbox_coder=dict(
+                    type='DeltaXYWHBBoxCoder',
+                    target_means=[0., 0., 0., 0.],
+                    target_stds=[0.033, 0.033, 0.067, 0.067]),
+                reg_class_agnostic=False,
+                reg_decoded_bbox=True,
+                norm_cfg=dict(type='BN', requires_grad=True), # Modified
+                loss_cls=dict(
+                    type='CrossEntropyLoss', use_sigmoid=False, loss_weight=1.0),
+                loss_bbox=dict(type='GIoULoss', loss_weight=10.0))
+        ]))
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+# Modified All
+# augmentation strategy originates from DETR / Sparse RCNN
+# train_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+#     dict(type='RandomFlip', flip_ratio=0.5),
+#     dict(type='AutoAugment',
+#          policies=[
+#              [
+#                  dict(type='Resize',
+#                       img_scale=[(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+#                                  (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+#                                  (736, 1333), (768, 1333), (800, 1333)],
+#                       multiscale_mode='value',
+#                       keep_ratio=True)
+#              ],
+#              [
+#                  dict(type='Resize',
+#                       img_scale=[(400, 1333), (500, 1333), (600, 1333)],
+#                       multiscale_mode='value',
+#                       keep_ratio=True),
+#                  dict(type='RandomCrop',
+#                       crop_type='absolute_range',
+#                       crop_size=(384, 600),
+#                       allow_negative_crop=True),
+#                  dict(type='Resize',
+#                       img_scale=[(480, 1333), (512, 1333), (544, 1333),
+#                                  (576, 1333), (608, 1333), (640, 1333),
+#                                  (672, 1333), (704, 1333), (736, 1333),
+#                                  (768, 1333), (800, 1333)],
+#                       multiscale_mode='value',
+#                       override=True,
+#                       keep_ratio=True)
+#              ]
+#          ]),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='Pad', size_divisor=32),
+#     dict(type='DefaultFormatBundle'),
+#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+# ]
+# data = dict(train=dict(pipeline=train_pipeline))
+
+optimizer = dict(_delete_=True, type='AdamW', lr=0.0001, betas=(0.9, 0.999), weight_decay=0.05,
+                 paramwise_cfg=dict(custom_keys={'absolute_pos_embed': dict(decay_mult=0.),
+                                                 'relative_position_bias_table': dict(decay_mult=0.),
+                                                 'norm': dict(decay_mult=0.)}))
+# lr_config = dict(step=[27, 33]) # Modified
+# runner = dict(type='EpochBasedRunner', max_epochs=36) # Modified
+
+# Modified All
+# do not use mmdet version fp16
+# fp16 = None
+# optimizer_config = dict(
+#     type="DistOptimizerHook",
+#     update_interval=1,
+#     grad_clip=None,
+#     coalesce=True,
+#     bucket_size_mb=-1,
+#     use_fp16=True,
+# )
+
+seed = 2022 # Modified
+gpu_ids = [0] # Modified
---
+++ prediction_thr_down.py
@@ -0,0 +1,64 @@
+from mmcv import Config
+from mmcv.runner import load_checkpoint
+from mmdet.models import build_detector
+from mmdet.apis import single_gpu_test
+from mmdet.datasets import build_dataloader, build_dataset
+from mmcv.parallel import MMDataParallel
+from pycocotools.coco import COCO
+import pandas as pd
+import argparse
+
+# Init
+
+parser = argparse.ArgumentParser()
+parser.add_argument('epoch', type=int)
+args = parser.parse_args()
+
+# Prediction
+
+RUN_NAME = "SwinTransformer_Pretrained"
+checkpoint_path = f"./epoch_{args.epoch}.pth"
+
+cfg = Config.fromfile('/opt/ml/detection/swin/configs/thr_down/modified_swin_base_thr_down.py')
+cfg.checkpoint_config = dict(max_keep_ckpts=50, interval=2)
+cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+cfg.log_config.hooks[1].init_kwargs.name = RUN_NAME
+
+model = build_detector(cfg.model)
+
+dataset = build_dataset(cfg.data.test)
+
+checkpoint = load_checkpoint(model, checkpoint_path, map_location='cpu')
+
+data_loader = build_dataloader(
+    dataset,
+    samples_per_gpu=cfg.data.samples_per_gpu,
+    workers_per_gpu=cfg.data.workers_per_gpu,
+    dist=False,
+    shuffle=False
+)
+
+model = MMDataParallel(model.cuda(), device_ids=[0])
+
+output = single_gpu_test(model, data_loader, show_score_thr=0.01)
+
+prediction_strings = []
+file_names = []
+
+coco = COCO(cfg.data.test.ann_file)
+img_ids = coco.getImgIds()
+
+for i, out in enumerate(output):
+    prediction_string = ''
+    image_info = coco.loadImgs(coco.getImgIds(imgIds=i))[0]
+    for j in range(10):
+        for o in out[j]:
+            prediction_string += str(j) + ' ' + str(o[4]) + ' ' + str(o[0]) + ' ' + str(o[1]) + ' ' + str(o[2]) + ' ' + str(o[3]) + ' '
+
+    prediction_strings.append(prediction_string)
+    file_names.append(image_info['file_name'])
+
+submission = pd.DataFrame()
+submission['PredictionString'] = prediction_strings
+submission['image_id'] = file_names
+submission.to_csv(f"./epoch{args.epoch}.csv", index=None)
