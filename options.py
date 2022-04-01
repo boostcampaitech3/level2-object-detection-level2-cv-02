1055ef6cb2049f8646dc1852b784ee501bfe348e
Juyoung Kim <ingressor531@gmail.com>
2022-04-01 17:49:51 +0900

    여러 코드에서 사용되는 메소드 분리

--- options.py
+++ options.py
@@ -4,6 +4,42 @@
 #
 
 
+from mmcv import Config
+
+from pycocotools.coco import COCO
+import pandas as pd
+
 RUN_NAME = 'SwinTransformer_HeavyAugs1_TTA1'
 CONFIG_PATH = '/opt/ml/detection/swin/configs/heavy_augs/modified_swin_base_heavy_augs.py'
 CONFIG_PATH_LOW_THR = '/opt/ml/detection/swin/configs/thr_down/modified_swin_base_heavy_augs_thr_down.py'
+
+
+def get_cfg(loc: str, run: str, epochs: int):
+    cfg = Config.fromfile(loc)
+    cfg.checkpoint_config = dict(max_keep_ckpts=100, interval=2)
+    cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+    cfg.log_config.hooks[1].init_kwargs.name = run
+    cfg.runner = dict(type='EpochBasedRunner', max_epochs=epochs)
+    return cfg
+
+
+def make_predictions(output, cfg, loc: str):
+    prediction_strings = []
+    file_names = []
+
+    coco = COCO(cfg.data.test.ann_file)
+
+    for i, out in enumerate(output):
+        prediction_string = ''
+        image_info = coco.loadImgs(coco.getImgIds(imgIds=i))[0]
+        for j in range(10):
+            for o in out[j]:
+                prediction_string += str(j) + ' ' + str(o[4]) + ' ' + str(o[0]) + ' ' + str(o[1]) + ' ' + str(o[2]) + ' ' + str(o[3]) + ' '
+
+        prediction_strings.append(prediction_string)
+        file_names.append(image_info['file_name'])
+
+    submission = pd.DataFrame()
+    submission['PredictionString'] = prediction_strings
+    submission['image_id'] = file_names
+    submission.to_csv(loc, index=None)
--- swin.py
+++ swin.py
@@ -10,50 +10,14 @@ from mmdet.models import build_detector
 from mmdet.apis import train_detector, single_gpu_test
 from mmdet.datasets import build_dataloader, build_dataset
 from mmcv.parallel import MMDataParallel
-from pycocotools.coco import COCO
 
-import os
-import pandas as pd
 import wandb
 
 from options import RUN_NAME, CONFIG_PATH, CONFIG_PATH_LOW_THR
-
-
-def get_cfg(loc: str, run: str, epochs: int):
-    cfg = Config.fromfile(loc)
-    cfg.checkpoint_config = dict(max_keep_ckpts=100, interval=2)
-    cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
-    cfg.log_config.hooks[1].init_kwargs.name = run
-    cfg.runner = dict(type='EpochBasedRunner', max_epochs=epochs)
-    return cfg
-
-
-def make_predictions(output, cfg, loc: str):
-    prediction_strings = []
-    file_names = []
-
-    coco = COCO(cfg.data.test.ann_file)
-
-    for i, out in enumerate(output):
-        prediction_string = ''
-        image_info = coco.loadImgs(coco.getImgIds(imgIds=i))[0]
-        for j in range(10):
-            for o in out[j]:
-                prediction_string += str(j) + ' ' + str(o[4]) + ' ' + str(o[0]) + ' ' + str(o[1]) + ' ' + str(o[2]) + ' ' + str(o[3]) + ' '
-
-        prediction_strings.append(prediction_string)
-        file_names.append(image_info['file_name'])
-
-    submission = pd.DataFrame()
-    submission['PredictionString'] = prediction_strings
-    submission['image_id'] = file_names
-    submission.to_csv(loc, index=None)
+from options import get_cfg, make_predictions
 
 
 if __name__ == '__main__':
-    # Experimental Hotfix
-    os.environ["WANDB_START_METHOD"] = "thread"
-
     # Init
     EPOCHS = 42
 
