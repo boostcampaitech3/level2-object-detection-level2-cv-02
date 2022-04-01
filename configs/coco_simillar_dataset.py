6bd74ea01766861374cf0293c9346c4779ea0503
Juyoung Kim <ingressor531@gmail.com>
2022-04-01 18:38:07 +0900

    HOTFIX: Preventing conversion from BGR to RGB

--- configs/coco_simillar_dataset.py
+++ configs/coco_simillar_dataset.py
@@ -3,8 +3,7 @@
 
 dataset_type = 'CocoDataset'
 data_root = '/opt/ml/detection/dataset/'
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=False)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
