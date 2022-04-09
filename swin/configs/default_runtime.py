d42806e21e97fa4a850c43eab06298db3be859bd
Juyoung Kim <ingressor531@gmail.com>
2022-04-09 15:00:52 +0900

    코드 정리

--- swin/configs/default_runtime.py
+++ swin/configs/default_runtime.py
@@ -2,19 +2,16 @@
 # https://github.com/SwinTransformer/Swin-Transformer-Object-Detection/blob/master/configs/_base_/default_runtime.py
 
 checkpoint_config = dict(max_keep_ckpts=100, interval=2)
-# yapf:disable
 log_config = dict(
     interval=50,
     hooks=[
         dict(type='TextLoggerHook'),
         dict(type='WandbLoggerHook',
             interval=100,
-            init_kwargs=dict(project='trash_detection_nestiank',
-                entity='bucket_interior'),
+            init_kwargs=dict(project='trash_detection_nestiank', entity='bucket_interior'),
             log_artifact=False
         )
     ])
-# yapf:enable
 custom_hooks = [dict(type='NumClassCheckHook')]
 
 dist_params = dict(backend='nccl')
